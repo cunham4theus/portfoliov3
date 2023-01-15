6c1d31045662b9317e8f1ff2d2fa16fedea4f2e6
cunham4theus <cunhamatheus.m4@gmail.com>
2023-01-15 02:05:50 -0300

    menu links bug fixed

--- assets/js/home.js
+++ assets/js/home.js
@@ -13,18 +13,10 @@ TweenMax.to('.home__overlay', 2, {
 });
 
 TweenMax.to('.nav', 2, {
-  delay: 2.2,  
+  delay: 2.3,  
   opacity: 1,
   ease: Expo.easeInOut,
 });
-
-TweenMax.from('.home__container--text h1', 1, {
-  delay: 3,
-  opacity: 0,
-  x: 200,
-  ease: Expo.easeInOut,
-});
-
 TweenMax.from('.home__container--text h1', 1, {
   delay: 3,
   opacity: 0,
@@ -40,7 +32,7 @@ TweenMax.from('.home__container--text p', 1, {
 });
 
 TweenMax.from('.ellipse-container', 1, {
-  delay: 3,
+  delay: 3.4,
   opacity: 0,
   ease: Expo.easeInOut,
 });
